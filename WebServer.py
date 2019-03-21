6accd369a48ed6cce91502251cb16d3e22b097ea
chezka-sino <chezka@pm.me>
2019-03-21 13:54:58 -0700

    will start from scratch

--- WebServer.py
+++ WebServer.py
@@ -1,12 +1,17 @@
 #import socket module
 from socket import *
 
-serverPort = 12000
+#source: http://joaoventura.net/blog/2017/python-webserver/
+
+serverPort = 8000
+serverHost = '0.0.0.0'
 serverSocket = socket(AF_INET, SOCK_STREAM)
 # TODO Prepare a server socket
 #Fill in start
-serverSocket.bind(('', serverPort))
+serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+serverSocket.bind((serverHost, serverPort))
 serverSocket.listen(1)
+print("Listening on  port %s ..." % serverPort)
 #Fill in end
 
 while True:
@@ -16,11 +21,12 @@ while True:
 
     try:
         # TODO check message
-        message = connectionSocket.recv(1024)
+        message = connectionSocket.recv(1024).decode()
+        print(message)
 
         # TODO uncomment after filling out message
-        # filename = message.split()[1]
-        #f = open(filename[1:]
+        filename = message.split()[1]
+        f = open(filename[1:])
 
         # TODO outputdata
         #outputdata = #Fill in start #Fill in end
