bf42f39621e591ba4eb61e6eab8d80c4b191c155
chezka-sino <chezka@pm.me>
2019-03-21 13:17:22 -0700

    init skeleton code for single threaded web server

---
+++ WebServer.py
@@ -0,0 +1,39 @@
+#import socket module
+from socket import *
+
+serverSocket = socket(AF_INET, SOCK_STREAM) #Prepare a sever socket
+#Fill in start
+#Fill in end
+
+while True:
+    #Establish the connection
+    print 'Ready to serve...'
+    # connectionSocket, addr =  #Fill in start #Fill in end
+
+    #try:
+        # TODO message =
+        # message = #Fill in start #Fill in end
+
+        # TODO uncomment after filling out message
+        # filename = message.split()[1]
+        #f = open(filename[1:]
+
+        # TODO outputdata
+        #outputdata = #Fill in start #Fill in end
+
+        # TODO Send one HTTP header line into socket
+        #Fill in start
+        #Fill in end
+
+        # TODO uncomment after filling out outputdata
+        # Send the content of the requested file to the client
+        #for i in range(0, len(outputdata)):
+        #    connectionSocket.send(outputdata[i])
+        #connectionSocket.close()
+    #except IOError:
+        # TODO Send response message for file not found
+        #Fill in start
+        #Fill in end
+        # TODO Close client socket
+        #Fill in start
+        #Fill in end
