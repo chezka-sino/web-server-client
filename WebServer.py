806c02541099a032d56023f86b18d8eabfabe92e
chezka-sino <chezka@pm.me>
2019-03-21 13:30:28 -0700

    socket

--- WebServer.py
+++ WebServer.py
@@ -1,18 +1,22 @@
 #import socket module
 from socket import *
 
-serverSocket = socket(AF_INET, SOCK_STREAM) #Prepare a sever socket
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_STREAM)
+# TODO Prepare a server socket
 #Fill in start
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
 #Fill in end
 
 while True:
     #Establish the connection
     print 'Ready to serve...'
-    # connectionSocket, addr =  #Fill in start #Fill in end
+    connectionSocket, addr =  serverSocket.accept()
 
-    #try:
-        # TODO message =
-        # message = #Fill in start #Fill in end
+    try:
+        # TODO check message
+        message = connectionSocket.recv(1024)
 
         # TODO uncomment after filling out message
         # filename = message.split()[1]
@@ -30,10 +34,13 @@ while True:
         #for i in range(0, len(outputdata)):
         #    connectionSocket.send(outputdata[i])
         #connectionSocket.close()
-    #except IOError:
+    except IOError:
+        print("placeholder")
         # TODO Send response message for file not found
         #Fill in start
         #Fill in end
         # TODO Close client socket
         #Fill in start
         #Fill in end
+
+serverSocket.close()
