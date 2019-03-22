c45c99748a9a40b96fe13eeb9b2609b869894cf7
chezka-sino <msino@dons.usfca.edu>
2019-03-21 21:48:21 -0700

    can handle single HTTP requests

--- WebServer.py
+++ WebServer.py
@@ -1,4 +1,3 @@
-#import socket module
 from socket import *
 
 # sources
@@ -8,47 +7,36 @@ from socket import *
 serverPort = 8000
 serverHost = '0.0.0.0'
 serverSocket = socket(AF_INET, SOCK_STREAM)
-# TODO Prepare a server socket
-#Fill in start
 serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 serverSocket.bind((serverHost, serverPort))
 serverSocket.listen(1)
 print("Listening on  port %s ..." % serverPort)
-#Fill in end
 
 while True:
-    #Establish the connection
-    print 'Ready to serve...'
-    connectionSocket, addr =  serverSocket.accept()
+    # Establish the connection
+    connectionSocket, addr = serverSocket.accept()
 
     try:
-        # TODO check message
+        # Get the client request
         message = connectionSocket.recv(1024).decode()
         print(message)
 
-        # TODO uncomment after filling out message
+        # Parse HTTP headers
         filename = message.split()[1]
-        f = open(filename[1:])
+        file = open(filename[1:])
 
-        # TODO outputdata
-        #outputdata = #Fill in start #Fill in end
+        outputdata = file.read()
+        file.close()
 
-        # TODO Send one HTTP header line into socket
-        #Fill in start
-        #Fill in end
+        response = 'HTTP/1.1 200 OK\n\n' + outputdata
 
-        # TODO uncomment after filling out outputdata
-        # Send the content of the requested file to the client
-        #for i in range(0, len(outputdata)):
-        #    connectionSocket.send(outputdata[i])
-        #connectionSocket.close()
+
     except IOError:
-        print("placeholder")
-        # TODO Send response message for file not found
-        #Fill in start
-        #Fill in end
-        # TODO Close client socket
-        #Fill in start
-        #Fill in end
+        errorFile = open('404.html')
+        errorOutput = errorFile.read()
+        response = 'HTTP/1.1 404 NOT FOUND\n\n' + errorOutput
+
+    connectionSocket.sendall(response.encode())
+    connectionSocket.close()
 
 serverSocket.close()
